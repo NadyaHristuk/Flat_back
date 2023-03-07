69890310a42367ef27b1c0e14358b90fc83da53e
nkhrystiuk <elhe2013@gmail.com>
2023-03-07 08:30:02 +0200

    fix

--- routes/personalPlan.js
+++ routes/personalPlan.js
@@ -6,6 +6,6 @@ router.post("/pre", authenticate, personalPlanCtrl.prePersonalPlan);
 router.post("/", authenticate, personalPlanCtrl.addPersonalPlan);
 router.get("/daily-limit", authenticate, personalPlanCtrl.getDailyLimit);
 router.get("/", authenticate, personalPlanCtrl.getPersonalPlan);
-router.patch("/", authenticate, personalPlanCtrl.patchPersonalPlan);
+router.put("/", authenticate, personalPlanCtrl.patchPersonalPlan);
 
 module.exports = router;
